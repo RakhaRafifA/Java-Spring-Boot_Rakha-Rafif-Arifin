4a6c808b4b1ac33621fe8b18867525bd231bbc04
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-18 21:02:01 +0700

    mengambil code dari stash

--- 2_Version Control and Branch Management (Git)/praktikum/PraktikumGit_Pertemuan1.java
+++ 2_Version Control and Branch Management (Git)/praktikum/PraktikumGit_Pertemuan1.java
@@ -2,5 +2,6 @@ class pertemuan1{
     public static void tugas1(String[] args) {
         System.out.println("Tugas Section 2 Version Control and Branch Management (Git)");
         System.out.println("Line code baris kedua");
+        System.out.println("Line code yang akan disimpan di stash");
     }
 }
