d914bc511cadc3068a59fba9e9693f5e18219e08
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-18 21:00:52 +0700

    menambahkan line code

--- 2_Version Control and Branch Management (Git)/praktikum/PraktikumGit_Pertemuan1.java
+++ 2_Version Control and Branch Management (Git)/praktikum/PraktikumGit_Pertemuan1.java
@@ -1,6 +1,6 @@
 class pertemuan1{
     public static void tugas1(String[] args) {
         System.out.println("Tugas Section 2 Version Control and Branch Management (Git)");
-        
+        System.out.println("Line code baris kedua");
     }
 }
