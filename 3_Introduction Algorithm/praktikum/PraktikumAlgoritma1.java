9edc3e9b3fc5a1ddad1ee877d572f762c4705b90
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-18 21:33:50 +0700

    test input

---
+++ 3_Introduction Algorithm/praktikum/PraktikumAlgoritma1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Aloritma1{
+    public static void prima(String[] args) {
+
+        Scanner bil = new Scanner(System.in);
+        System.out.println("Masukan bilangan 1 - 9 = ");
+        int a = bil.nextInt();
+        System.out.println("Bilangan anda = " + a);
+
+        bil.close();
+    }
+}
