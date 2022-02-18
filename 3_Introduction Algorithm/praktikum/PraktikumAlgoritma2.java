2b3e3a890fe8defb6cb72c6e21d6b0a285cbeb5e
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-18 21:59:13 +0700

    input code algoritma2

---
+++ 3_Introduction Algorithm/praktikum/PraktikumAlgoritma2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Algoritma2{
+    public static void lampu() {
+
+        Scanner baris = new Scanner(System.in);
+        System.out.println("Masukan jumlah baris = ");
+        int a = baris.nextInt();
+        System.out.println("Anda memasukan jumlah baris sebanyak = " + a);
+        baris.close();
+    }
+}
