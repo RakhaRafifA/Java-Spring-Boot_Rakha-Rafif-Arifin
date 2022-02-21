817a2f0b1a768777aff56d053d200a00adf23748
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-21 12:27:43 +0700

    perbaikan tempat for dan if

---
+++ 4_Basic Programming/praktikum/PraktikumPalindrome.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+class Palindrome{
+    public static void main(String[] args) {
+        String kata, balik = "";
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan kata yang akan dicari = ");
+        kata = input.nextLine();
+        int length = kata.length();
+        input.close();
+        kata.toLowerCase();
+
+        for(int i = length -1; i >= 0; i--){
+            balik = balik + kata.charAt(i);
+        }
+        if(kata == balik){
+            System.out.println("Palindrome");
+        }
+        else if(kata != balik){
+            System.out.println("Bukan Palindrome");
+        }
+    }
+}
