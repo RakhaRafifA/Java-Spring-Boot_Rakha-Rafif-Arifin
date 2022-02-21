1d936bbd9c1198798f47d4595e2d1412f2450cdf
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-21 11:07:11 +0700

    Praktikum mencari faktor bilangan

---
+++ 4_Basic Programming/praktikum/PraktikumFaktorBilangan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class FaktorBilangan{
+    public static void main(String[] args) {
+        int bilangan;
+
+        Scanner bil = new Scanner(System.in);
+        System.out.println("Masukan Bilangan yang akan di cari = ");
+        bilangan = bil.nextInt();
+        System.out.println("Bilangan yang anda cari = " + bilangan);
+        bil.close();
+
+        for (int i = 0; i<bilangan; i++){
+            if(bilangan%i == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
