9f4de75c7cfbab8f1625901b32e7ed5de46ad1b7
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-21 11:35:02 +0700

    menghapus import yang tidak terpakai

---
+++ 4_Basic Programming/praktikum/PraktikumBilanganPrima.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Prima{
+    public static void main(String[] args) {
+        int bilangan;
+        
+        Scanner prima = new Scanner(System.in);
+        System.out.println("Masukan bilangan yang ingin anda cari dari 1 - 13 = ");
+        bilangan = prima.nextInt();
+        System.out.println("Bilangan yang anda cari = " + bilangan);
+        prima.close();
+
+        if(bilangan<=4){
+            System.out.println("Bilangan Prima");
+        }
+        else if(bilangan >= 4 && bilangan%2 == 0){
+            System.out.println("Bukan Bilangan Prima");
+        }
+        else if(bilangan >= 4 || bilangan<=13 || bilangan%2 != 0){
+            System.out.println("Bilangan Prima");
+        }
+        else if(bilangan>13){
+            System.out.println("Bilangan yang anda masukan tidak bisa dicari");
+        }
+    }
+}
