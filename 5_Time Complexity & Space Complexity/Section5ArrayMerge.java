1417ad24c5ddee67b960db73adc92df22b966bdf
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-23 13:50:39 +0700

    Praktikum Section 5 Array Merge

--- 5_Time Complexity & Space Complexity/Section5ArrayMerge.java
+++ 5_Time Complexity & Space Complexity/Section5ArrayMerge.java
@@ -1,8 +1,30 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class ArrayMerge{
     public static void main(String[] args) {
-        System.out.println(ArrayMerge(String[] array1={"kazuya", "jin", "lee"}; String[] array2={"kazuya", "feng"}));
-        System.out.println(ArrayMerge(String[] array1={"lee", "jin"}, String[] array2={"kazuya", "panda"}));
-        
+      String[] arr1={"kazuya", "jin", "lee"};
+      String[] arr2={"kazuya", "feng"};
+      String[] arr3={"lee", "jin"};
+      String[] arr4={"kazuya", "panda"};
+      Array(arr1, arr2);
+      System.out.println(" ");
+      Array(arr3, arr4);
+}
+    public static void Array(String[] arr1, String[] arr2){
+        Set finalarray = new HashSet<Array>();
+        finalarray.addAll(Arrays.asList(arr1));
+        finalarray.addAll(Arrays.asList(arr2));
+
+        String[] finalarrayset = new String[finalarray.size()];
+        finalarray.toArray(finalarrayset);
+
+        Arrays.sort(finalarrayset);
+
+        for (String str : finalarrayset){
+            System.out.print(str + " ");
+        }
     }
-    static ArrayMerge
 }
