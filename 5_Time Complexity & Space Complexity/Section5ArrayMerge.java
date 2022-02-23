ef1d064de90eb33a3f7708a7d681be95f1d380c3
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-23 11:38:03 +0700

    test

---
+++ 5_Time Complexity & Space Complexity/Section5ArrayMerge.java
@@ -0,0 +1,8 @@
+class ArrayMerge{
+    public static void main(String[] args) {
+        System.out.println(ArrayMerge(String[] array1={"kazuya", "jin", "lee"}; String[] array2={"kazuya", "feng"}));
+        System.out.println(ArrayMerge(String[] array1={"lee", "jin"}, String[] array2={"kazuya", "panda"}));
+        
+    }
+    static ArrayMerge
+}
