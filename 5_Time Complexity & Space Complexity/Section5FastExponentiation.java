b1147a201d27bd5633e52b5ccc3f02d085dfd642
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-23 10:47:30 +0700

    Praktikum Section 5 Fast Exponentation

---
+++ 5_Time Complexity & Space Complexity/Section5FastExponentiation.java
@@ -0,0 +1,15 @@
+class FastExponentation{
+
+    public static void main(String[] args) {
+        System.out.println(pow(2, 3));  // 8
+        System.out.println(pow(5, 3));  // 125
+        System.out.println(pow(10, 2)); // 100
+        System.out.println(pow(2, 5)); // 32
+        System.out.println(pow(7, 3));  // 343
+        System.out.println(pow(7, 2));  // 49
+    }
+    
+    static Integer pow(Integer x, Integer n) {
+        return (int) Math.pow(x, n);
+    }
+    }
