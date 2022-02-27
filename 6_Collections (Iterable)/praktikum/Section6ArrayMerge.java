2033819a478c6c9fe83ccf148e139319bcfbe9ce
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-27 20:36:42 +0700

    section 6 array merge

---
+++ 6_Collections (Iterable)/praktikum/Section6ArrayMerge.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+class S6Problem1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Nama untuk array1= ");
+        String m = input.nextLine();
+        String n = input.nextLine();
+        String o = input.nextLine();
+        String[] arr1 = {m, n, o};
+        System.out.println("Masukan Nama untuk array2= ");
+        String p = input.nextLine();
+        String q = input.nextLine();
+        String r = input.nextLine();
+        String[] arr2 = {p, q, r};
+        System.out.println("Array yang anda masukan = ");
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("Array setelah digabung = ");
+        Arrayss(arr1, arr2);
+        input.close();
+    }
+    static void Arrayss(String[] array1, String[] array2){
+        Set <String> finalarray = new HashSet<>();
+        finalarray.addAll(Arrays.asList(array1));
+        finalarray.addAll(Arrays.asList(array2));
+
+        String[] gabungan = new String[finalarray.size()];
+        finalarray.toArray(gabungan);
+
+        System.out.println(Arrays.toString(gabungan));
+    }
+}
