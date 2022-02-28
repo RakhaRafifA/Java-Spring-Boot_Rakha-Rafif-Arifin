59b77485c9cffad92060ef2794a22f70c84afede
RakhaRafifArifin <rakhara104@gmail.com>
2022-02-28 13:01:14 +0700

    Source code Maximum sum of Subarray

---
+++ 6&7_Collection/praktikum/Section6MaximumSumSubArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class S6Problem6{
+    public static void main(String[] args) {
+        int angka = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan Angka yang ingin anda masukan ke dalam array = ");
+        angka = input.nextInt();
+        System.out.println("Masukan K = ");
+        int k = input.nextInt();
+        String temp = Integer.toString(angka);
+        int[] number = new int[temp.length()];
+        for(int i = 0; i < temp.length(); i++){
+            number[i] = temp.charAt(i) - '0';
+        }
+        System.out.println(Arrays.toString(number));
+        int a = number.length;
+        input.close();
+        Maxsum(number, a, k);
+    }
+    static int Maxsum(int[] arr, int n, int k){
+        if(n<k){
+            System.out.println("Tidak dapat mencari, K lebih besar dari array");
+            return -1;
+        }
+        int hasil = 0;
+        for(int i = 0; i<k; i++){
+            hasil = hasil + arr[i];
+        }
+        int temp = hasil;
+        for(int i = k; i < n; i++){
+            temp = temp + arr[i] - arr[i - k];
+            hasil = Math.max(hasil, temp);
+        }
+        return hasil;
+    }
+}
