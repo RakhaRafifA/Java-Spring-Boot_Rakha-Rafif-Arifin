166168917f85f43f8b963699ae5b156732df01e8
RakhaRafifArifin <rakhara104@gmail.com>
2022-03-01 09:56:55 +0700

    menambahkan variabel min dan max, dan memperbaiki kode if

--- 8_Recursive-Number Theory-Sorting and Searching/praktikum/Section8MinMaxNumber.java
+++ 8_Recursive-Number Theory-Sorting and Searching/praktikum/Section8MinMaxNumber.java
@@ -1,17 +1,22 @@
 class S8Problem3{
     public static void main(String[] args) {
-        int[] array1 = {5, 7, 4, -2, -1, -8};
+        int[] array1 = {5, 7, 4, -2, -1, 8};
         int[] array2 = {2, -5, -4, 22, 7, 7};
         MinMax(array1);
         MinMax(array2);
         
     }
     static int MinMax(int[] arr){
+        int min = arr[0], max = arr[0];
         for(int i = 0; i < arr.length; i++){
-            if(arr[i]<arr[i+1]){
-                System.out.println(arr[i]);
+            if(arr[i]<min){
+                min = arr[i];
+            }
+            if(arr[i]>max){
+                max = arr[i];
             }
         }
+        System.out.println("min = " + min + "max = " + max);
         return -1;
     }
 }
