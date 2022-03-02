d79473b2f70e4884285a7d4e6d9643915601da57
RakhaRafifArifin <rakhara104@gmail.com>
2022-03-02 16:03:07 +0700

    perbaikan dan penambahan method

--- 9_Problem Solving Brute Force/praktikum/Section9DragonofLoowater.java
+++ 9_Problem Solving Brute Force/praktikum/Section9DragonofLoowater.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 class S9Problem3{
     public static void main(String[] args) {
-
+       
         Scanner input = new Scanner(System.in);
         System.out.println("Masukan Size array dragon = ");
         int a = input.nextInt();
@@ -22,5 +22,30 @@ class S9Problem3{
         input.close();
         System.out.println("Array Dragon = " + Arrays.toString(dragon));
         System.out.println("Array Knight = " + Arrays.toString(knight));
+        LooWater(dragon, knight);
+    }
+    static int LooWater(int[] arr1, int[] arr2){
+        int naga = 0, ksatria = 0, temp = 0;
+        for(int i = 0; i < arr1.length; i++){
+            naga += arr1[i];
+        }
+        for(int i = 0; i < arr2.length; i++){
+            for(int j = arr2.length - 1 ; j > i; j--){
+                temp = arr2[i] + arr2[j];
+                if(temp >= naga){
+                    ksatria = temp;
+                    if(temp <= ksatria){
+                        ksatria = temp;
+                    }
+                }
+            }
+        }
+        if(naga <= ksatria){
+            System.out.println(ksatria);
+        }
+        else{
+            System.out.println("Knight Fall");
+        }
+        return ksatria;
     }
 }
