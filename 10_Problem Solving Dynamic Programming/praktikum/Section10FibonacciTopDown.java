16098ab196b4d18b8f4a784281478d29112f4edf
RakhaRafifArifin <rakhara104@gmail.com>
2022-03-03 11:59:48 +0700

    perbaikan code fibo topdown

--- 10_Problem Solving Dynamic Programming/praktikum/Section10FibonacciTopDown.java
+++ 10_Problem Solving Dynamic Programming/praktikum/Section10FibonacciTopDown.java
@@ -7,18 +7,16 @@ class S10FibTopDown{
         int x = input.nextInt();
         int[] fibo = new int[x + 1];
         input.close();
-        TopDown(fibo, x);
+        System.out.println(TopDown(x, fibo));
     }
-    static int[] TopDown(int[] fib, int n){
-        int result = 0;
-        if(n == 0 || n == 1){
-            return n;
-        }
-        if(arr[n] != 0){
-            return fib[n];
-        }
-        fib[n] = fib[n-1] + fib[n-2];
-        System.out.println(fib[n]);
-        return fib[n];
+    static int TopDown(int n, int[] arr){
+            if(n <= 1){
+                return n;
+            }
+            if(arr[n] != 0 ){
+                return arr[n];
+            }
+            arr[n] = TopDown(n - 1, arr) + TopDown(n - 2, arr);
+            return arr[n];
     }
 }
