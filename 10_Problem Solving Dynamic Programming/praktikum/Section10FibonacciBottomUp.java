909b470fb1366bd52d7b4604749ea01f7d063129
RakhaRafifArifin <rakhara104@gmail.com>
2022-03-03 12:02:06 +0700

    Source code fibonacci bottom up

---
+++ 10_Problem Solving Dynamic Programming/praktikum/Section10FibonacciBottomUp.java
@@ -0,0 +1,23 @@
+class S10Problem2{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukan barisan ke berapa yang anda cari = ");
+        int x = input.nextInt();
+        input.close();
+        System.out.println(BottomUp(x));
+    }
+    static int BottomUp(int n){
+        int a = 0;
+        int b = 1;
+        int temp = 0;
+        if(n <= 1){
+            return n;
+        }
+        for(int i = 2; i <= n; i++){
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return b;
+    }
+}
