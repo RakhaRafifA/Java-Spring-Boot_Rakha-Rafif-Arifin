929d3770394c384b9255595b5f3030cb70cd1f13
RakhaRafifArifin <rakhara104@gmail.com>
2022-03-07 13:29:21 +0700

    perbaikan code inheritance vehicles

--- 11_Generic & OOP 1/praktikum/Section11InheritancePolymorphismVehice.java
+++ 11_Generic & OOP 1/praktikum/Section11InheritancePolymorphismVehice.java
@@ -4,50 +4,66 @@ import java.util.Scanner;
 class Vehicle{
     String name = "Gerobak";
     String with_engine = "'No Engine'";
+    public void identify_myself(){
+        System.out.println("Hi I'm Parent of All Vehicles , My Name is " + name + ", My Engine Status is " + with_engine + "\n");
+    }
 }
 
 class Bikes extends Vehicle{
     int wheel_count = 2;
+    public void identify_myself(){
+        System.out.println("Hi I'm Bike , My Name is " + name + ", My Engine Status is " + with_engine + ", I Have " + wheel_count + " Wheels");
+    }
 }
 class Cars extends Vehicle{
     int wheel_count = 4;
-    String engine_type = "V8";
+    String engine_type;
+    public void identify_myself(){
+        System.out.println("Hi I'm Cars , My Name is " + name + ", My Engine Status is " + with_engine + ", I Have " + wheel_count + " Wheels" + ", My Engine Type = " + engine_type + "");
+    }
 }
 class Busses extends Vehicle{
     int wheel_count = 4;
-    String private_bus = "[Public Bus]";
+    String private_bus;
+    public void identify_myself(){
+        System.out.println("Hi I'm " + private_bus + " , My Name is " + name + ", My Engine Status is " + with_engine + ", I Have " + wheel_count + " Wheels");
+    }
 }
 
 public class Section11InheritancePolymorphismVehice {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         Vehicle v = new Vehicle();
-        System.out.println("Hi I'm Parent of All Vehicles , My Name is " + v.name + ", My Engine Status is " + v.with_engine + "\n");
+        v.identify_myself();
 
         Bikes b = new Bikes();
         b.name = "Pedal Bikes";
-        System.out.println("Hi I'm Bike , My Name is " + b.name + ", My Engine Status is " + b.with_engine + ", I Have " + b.wheel_count + " Wheels");
+        b.identify_myself();
         b.name = "Motor bikes";
         b.with_engine = "'With Engine'";
-        System.out.println("Hi I'm Bike , My Name is " + b.name + ", My Engine Status is " + b.with_engine + ", I Have " + b.wheel_count + " Wheels\n");
+        b.identify_myself();
+        System.out.println();
 
         Cars c = new Cars();
         c.name = "Sport Cars";
         c.with_engine = "'With Engine'";
-        System.out.println("Hi I'm Cars , My Name is " + c.name + ", My Engine Status is " + c.with_engine + ", I Have " + c.wheel_count + " Wheels" + ", My Engine Type = " + c.engine_type + "");
+        c.engine_type = "V 8";
+        c.identify_myself();
         c.name = "Pickup Cars";
         c.with_engine = "'With Engine'";
         c.engine_type = "solar";
-        System.out.println("Hi I'm Cars , My Name is " + c.name + ", My Engine Status is " + c.with_engine + ", I Have " + c.wheel_count + " Wheels" + ", My Engine Type = " + c.engine_type + "\n");
+        c.identify_myself();
+        System.out.println();
         
         Busses bus = new Busses();
         bus.name = "Trans Jakarta";
         bus.with_engine = "'With Engine'";
-        System.out.println("Hi I'm " + bus.private_bus + " , My Name is " + bus.name + ", My Engine Status is " + bus.with_engine + ", I Have " + bus.wheel_count + " Wheels");
+        bus.private_bus = "[Public Bus]";
+        bus.identify_myself();
         bus.name = "School Bus";
         bus.with_engine = "'With Engine'";
         bus.private_bus = "[Private Engine]";
-        System.out.println("Hi I'm " + bus.private_bus + " , My Name is " + bus.name + ", My Engine Status is " + bus.with_engine + ", I Have " + bus.wheel_count + " Wheels");
+        bus.identify_myself();
         
         input.close();
     }
