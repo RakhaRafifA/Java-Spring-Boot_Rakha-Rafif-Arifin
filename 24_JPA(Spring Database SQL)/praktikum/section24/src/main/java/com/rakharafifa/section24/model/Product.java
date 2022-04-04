0e216b6db1c0748c27505ebd75bd0d220cdff4e8
RakhaRafifArifin <rakhara104@gmail.com>
2022-04-04 14:11:32 +0700

    update

--- 24_JPA(Spring Database SQL)/praktikum/section24/src/main/java/com/rakharafifa/section24/model/Product.java
+++ 24_JPA(Spring Database SQL)/praktikum/section24/src/main/java/com/rakharafifa/section24/model/Product.java
@@ -6,36 +6,17 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
-@Getter
-@Setter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Long price;
-    
-    public String getName() {
-        return name;
-    }
-    public Long getPrice() {
-        return price;
-    }
-    public void setPrice(Long price) {
-        this.price = price;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
 }
